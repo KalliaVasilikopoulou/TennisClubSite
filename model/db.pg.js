94387c0ac084b79e910c2fddcde6d8471cdc9715
KalliaVasilikopoulou <98029855+KalliaVasilikopoulou@users.noreply.github.com>
2022-07-05 17:58:53 +0300

    release version tennis club v2.0
    latest updates on tennis club site (added also unit test for controllers).

---
+++ model/db.pg.js
@@ -0,0 +1,19 @@
+'use strict';
+const { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    idleTimeoutMillis: 0,
+    connectionTimeoutMillis: 0
+});
+
+client.connect((err) => {
+    if (err)
+        throw err;
+});
+
+module.exports = client
