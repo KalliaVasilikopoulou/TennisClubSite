32f220eb196ceab8dd36b3d29e3223647eee9846
KalliaVasilikopoulou <98029855+KalliaVasilikopoulou@users.noreply.github.com>
2022-07-06 17:35:32 +0300

    connection to heroku db

--- model/db.pg.js
+++ model/db.pg.js
@@ -2,13 +2,14 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-    idleTimeoutMillis: 0,
-    connectionTimeoutMillis: 0
+    connectionString: process.env.DATABASE_URL
+    // user: process.env.PG_USER,
+    // host: process.env.PG_HOST,
+    // database: process.env.PG_DATABASE,
+    // password: process.env.PG_PASSWORD,
+    // port: process.env.PG_PORT,
+    // idleTimeoutMillis: 0,
+    // connectionTimeoutMillis: 0
 });
 
 client.connect((err) => {
