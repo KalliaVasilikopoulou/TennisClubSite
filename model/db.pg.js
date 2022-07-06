2f9671e5890b8fa88a0583620d9f1c8bb5372419
KalliaVasilikopoulou <98029855+KalliaVasilikopoulou@users.noreply.github.com>
2022-07-06 17:42:03 +0300

    update on db heroku

--- model/db.pg.js
+++ model/db.pg.js
@@ -2,7 +2,8 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
     // user: process.env.PG_USER,
     // host: process.env.PG_HOST,
     // database: process.env.PG_DATABASE,
